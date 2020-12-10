3ef4ac1a7ffe1d01a7f626123b1c712daa78d992
leenapark <hrn424@gmail.com>
2020-12-10 23:16:21 +0900

    20201210 연습 문제 eqauls() hashCode() 이용

---
+++ src/com/javaex/ex07/Point.java
@@ -0,0 +1,40 @@
+package com.javaex.ex07;
+
+public class Point {
+	
+	//필드
+	private int x;
+	private int y;
+	
+	//생성자
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	//equals() 메소드를 작성하세요.
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (x != other.x)
+			return false;
+		return true;
+	}
+
+}
